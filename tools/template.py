700bb271c3531ba137ad70ab58ac8a8cbefed3a7
ReiAshizawa <rei11a2004@gmail.com>
2024-06-10 21:01:17 +0900

    use filepath

--- tools/template.py
+++ tools/template.py
@@ -1,15 +1,16 @@
 import pwn
+filepath = "./vuln"
 pwn.context.arch = "amd64"
 pwn.context.bits = 64
 # pwn.context.log_level = "DEBUG"
-# io = pwn.process("./vuln")
+# io = pwn.process(filepath)
 # io: pwn.tubes.tube.tube = pwn.gdb.debug(
-#     "./vuln", """
+#     "filepath", """
 #     b *main
 #     """,env={"LD_PRELOAD":"./libc.so.6"}
 # )
 io = pwn.remote("", )
-e = pwn.ELF("./vuln")
+e = pwn.ELF(filepath)
 # libc = pwn.ELF("./libc.so.6")
 
 io.interactive()
