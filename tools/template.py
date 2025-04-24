0c791465f898a1678261faf4326f05cf9b6195d8
ReiAshizawa <rei11a2004@gmail.com>
2025-04-24 19:58:56 +0900

    update template

--- tools/template.py
+++ tools/template.py
@@ -2,14 +2,16 @@ import pwn
 filepath = "./vuln"
 pwn.context.arch = "amd64"
 pwn.context.bits = 64
+pwn.context.terminal = "tmux splitw -h".split()
 # pwn.context.log_level = "DEBUG"
 # io = pwn.process(filepath)
 # io: pwn.tubes.tube.tube = pwn.gdb.debug(
-#     "filepath", """
+#     [filepath], """
 #     b *main
 #     """,env={"LD_PRELOAD":"./libc.so.6"}
 # )
 io = pwn.remote("", )
+# pwn.util.proc.wait_for_debugger(pwn.util.proc.pidof(io)[0])
 e = pwn.ELF(filepath)
 # libc = pwn.ELF("./libc.so.6")
 
